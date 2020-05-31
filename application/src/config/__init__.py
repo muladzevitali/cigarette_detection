7349a022212d04c591f4e897870a811058bb7d3f
Vitali Muladze <='vmuladze@bog.ge>
2020-05-31 22:19:23 +0400

    bug in configuration

--- application/src/config/__init__.py
+++ application/src/config/__init__.py
@@ -5,7 +5,7 @@ from .config_parser import (Application, FaissConfiguration, Media,
 app_config = Application
 database_config = ApplicationDatabase
 faiss_config = FaissConfiguration
-media_config = Media
+media_config = Media()
 yolo_config = YoloConfiguration
 # Prefix for all tables in the app context
 table_prefix = database_config.table_prefix
